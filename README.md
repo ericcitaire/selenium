4013e1dd60b1ac120d6220ec90d53c6663414f97
ericcitaire <eric.citaire@gmail.com>
2022-01-30 16:06:50 +0000

    Parameterized tests, in parallel

--- README.md
+++ README.md
@@ -1,6 +1,44 @@
 ```
 docker network create selenium
-docker run --network selenium --name chrome -d --shm-size="2g" selenium/standalone-chrome:4.1.1-20220121
-docker run --network selenium --name sample -d -w $pwd/static -v $(pwd):$(pwd) python:3 python -m http.server
-docker run --network selenium --rm -it -w $(pwd) -v $(pwd):$(pwd) maven:3-openjdk-11 mvn -ntp clean install
+docker run \
+  --network selenium \
+  --name selenium-hub \
+  -d \
+  -p 4442-4444:4442-4444 \
+  selenium/hub:4.1.1-20220121
+docker run \
+  --network selenium \
+  --name chrome \
+  -d --shm-size="2g" \
+  -e SE_EVENT_BUS_HOST=selenium-hub \
+  -e SE_EVENT_BUS_PUBLISH_PORT=4442 \
+  -e SE_EVENT_BUS_SUBSCRIBE_PORT=4443 \
+  selenium/node-chrome:4.1.1-20220121
+docker run \
+  --network selenium \
+  --name firefox \
+  -d --shm-size="2g" \
+  -e SE_EVENT_BUS_HOST=selenium-hub \
+  -e SE_EVENT_BUS_PUBLISH_PORT=4442 \
+  -e SE_EVENT_BUS_SUBSCRIBE_PORT=4443 \
+  selenium/node-firefox:4.1.1-20220121
+docker run \
+  --network selenium \
+  --name edge \
+  -d --shm-size="2g" \
+  -e SE_EVENT_BUS_HOST=selenium-hub \
+  -e SE_EVENT_BUS_PUBLISH_PORT=4442 \
+  -e SE_EVENT_BUS_SUBSCRIBE_PORT=4443 \
+  selenium/node-edge:4.1.1-20220121
+docker run \
+  --network selenium \
+  --name sample \
+  -d \
+  -w $(pwd)/static -v $(pwd):$(pwd) \
+  python:3 python -m http.server
+docker run \
+  --network selenium \
+  --rm -it \
+  -w $(pwd) -v $(pwd):$(pwd) \
+  maven:3-openjdk-11 mvn -ntp clean install
 ```
--- src/test/java/UiTest.java
+++ src/test/java/UiTest.java
@@ -6,22 +6,50 @@ import java.time.Duration;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(value = Parameterized.class)
 public class UiTest {
+    private Capabilities capabilities;
     private WebDriver driver;
     private final By btn = By.id("btn");
     private final By content = By.id("content");
 
+    public UiTest(Capabilities capabilities) {
+        this.capabilities = capabilities;
+    }
+
+    @Parameters(name = "Browser {0}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {new ChromeOptions()},
+                {new FirefoxOptions()},
+                {new EdgeOptions()}
+        });
+    }
+
     @Before
     public void setup() throws Exception {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        this.driver = new RemoteWebDriver(new URL("http://chrome:4444"), chromeOptions);
+        this.driver = new RemoteWebDriver(new URL("http://selenium-hub:4444"), this.capabilities);
     }
 
     @After
